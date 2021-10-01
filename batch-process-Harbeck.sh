6945b401f25d4f9e897312bde34db0d7dc98b695
JarOfCans <johnharbeck3.14@gmail.com>
2021-10-01 11:51:07 -0500

    Update 1.0
    Adds Word count, updates file size line

--- batch-process-Harbeck.sh
+++ batch-process-Harbeck.sh
@@ -2,8 +2,10 @@
 for f in "$1"/*; do
   if [ -f "$f" ]; then
     SIZE="$(du -sh "${f}" | cut -f1)"
+	WORD_COUNT="$(wc -w "${f}" | cut -d' ' -f1)"
     echo "Processing $f file..."
-    echo "$SIZE"
+    echo "File size: $SIZE"
+	echo "Word count: $WORD_COUNT"
   fi
 done
 
